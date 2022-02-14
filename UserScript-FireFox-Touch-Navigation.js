88bbba9167dbfb4e2af5d870e447cee34158c05e
ToFab <tony@fabian.dk>
2022-02-14 16:39:13 +0700

    Fixed error stopped listning for touch events

--- UserScript-FireFox-Touch-Navigation.js
+++ UserScript-FireFox-Touch-Navigation.js
@@ -6,38 +6,32 @@
 // @author       Tony Fabian
 // @include      *://*/*
 // @grant        none
-// @noframes
 // @license MIT
+// @noframes
 // ==/UserScript==
 (function () {
-    window.addEventListener('load', function () {
-        var bodyTag = document.getElementsByTagName("body");
-        if (bodyTag != null) {
-            var body = bodyTag[0];
-            if (body != null) {
-                var clientX, clientY;
-                body.addEventListener('touchstart', function (e) {
-                    // Cache the client X/Y coordinates
-                    clientX = e.touches[0].clientX;
-                    clientY = e.touches[0].clientY;
-                }, false);
-                body.addEventListener('touchend', function (e) {
-                    var deltaX, deltaY;
-                    // Compute the change in X and Y coordinates.
-                    // The first touch point in the changedTouches
-                    // list is the touch point that was just removed from the surface.
-                    deltaX = e.changedTouches[0].clientX - clientX;
-                    deltaY = e.changedTouches[0].clientY - clientY;
-                    if (deltaX <= -75) {
-                        console.log("navigate back");
-                        history.back();
-                    }
-                    if (deltaX >= 75) {
-                        console.log("navigate forward");
-                        history.forward();
-                    }
-                }, false);
-            }
+    // Register touchstart and touchend listeners for element 'source'
+    var src = document.getElementsByTagName("body")[0];
+    var clientX, clientY;
+    src.addEventListener('touchstart', function (e) {
+        // Cache the client X/Y coordinates
+        clientX = e.touches[0].clientX;
+        clientY = e.touches[0].clientY;
+    }, false);
+    src.addEventListener('touchend', function (e) {
+        var deltaX, deltaY;
+        // Compute the change in X and Y coordinates.
+        // The first touch point in the changedTouches
+        // list is the touch point that was just removed from the surface.
+        deltaX = e.changedTouches[0].clientX - clientX;
+        deltaY = e.changedTouches[0].clientY - clientY;
+        if (deltaX <= -75) {
+            console.log("navigate back");
+            history.back();
         }
-    });
+        if (deltaX >= 75) {
+            console.log("navigate forward");
+            history.forward();
+        }
+    }, false);
 })();
